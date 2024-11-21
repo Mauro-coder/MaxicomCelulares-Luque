1e100a85172b630512d2b18956fb3a271cb21251
Mauro Luque <mauroluque72@gmail.com>
2024-11-21 19:19:13 -0300

    add products in data.jsx

--- src/mock/data.jsx
+++ src/mock/data.jsx
@@ -34,6 +34,60 @@ const productos = [
         description: 'Diseño exclusivo con pantalla flexible de 6.9” y bisagra “gota” sin espacios entre sus lados.',
         img: '/motorola_razer.webp',
         category: 'celulares'
+    },
+    {
+        id:'5',
+        name:'Auriculares MotoBud',
+        stock: 25,
+        price: 74999,
+        description: 'Mayor Duración de Bateria.',
+        img: '/moto_bud.png',
+        category: 'accesorios'
+    },
+    {
+        id:'6',
+        name:'Auriculares Galaxy Buds Color Grafito',
+        stock: 15,
+        price: 144200,
+        description: 'Descubre la excelencia en sonido con los Galaxy Buds Fe Color Grafito, una creación de la reconocida marca Samsung.',
+        img: '/samsung_buds.webp',
+        category: 'accesorios'
+    },
+    {
+        id:'7',
+        name:'Cargador Inalambrico Celular Con Parlante',
+        stock: 20,
+        price: 27840,
+        description: 'Novedosa Lampara 3 en 1, Iluminacion + Base de Carga + Parlante.',
+        img: '/cargador.webp',
+        category: 'accesorios'
+    },
+    {
+        id:'8',
+        name:'SmartWatch K37 Gps Negro',
+        stock: 6,
+        price: 110000,
+        description: 'El SmartWatch se vincula con la app "InfoWear", la misma se encuentra disponible tanto para android como iOS.',
+        img: '/smart1.webp',
+        category: 'articulos varios'
+    },
+    {
+        id:'9',
+        name:'Smartwatch Redmi Watch 5 Active Blanco',
+        stock: 3,
+        price: 85000,
+        description: 'Los datos del ritmo cardíaco en tiempo real las 24hs son más precisos con el sensor mejorado. Puedes revisar los cambios en el ritmo cardíaco mediante el reloj y el teléfono móvil.',
+        img: '/smart2.webp',
+        category: 'articulos varios'
+    },
+    {
+        id:'10',
+        name:'Smartwatch HK10 Ultra 3 Wifi Plateado',
+        stock: 7,
+        price: 130000,
+        description: 'Una de las mayores virtudes que presenta este modelo es la posibilidad de escuchar música sin necesidad de llevar tu celular en la mano.',
+        img: '/smart3.webp',
+        category: 'articulos varios'
     }
 ]
 
